d185afa8fb07a8fcc43c7c35fa1d98120a6c32a8
unknown <przetacznikp@gmail.com>
2021-02-16 18:59:06 +0100

    small improvements

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -9,23 +9,25 @@ import { ReactComponent as UserIcon } from '../../assets/icons/user-circle.svg';
 
 export default function Navbar() {
     const [location, setLocation] = useLocation();
+    const style = {cursor: "pointer"};
 
     return (
         <div className="nav-bar">
             <div className="title-bar">
-                <div className="title">
+                <div className="title" onClick={()=> setLocation("/")}
+                style={style}>
                     <span>NMAE</span>
                 </div>
                 <div>share your experiences</div>
             </div>
             <div style={{ display: 'flex' }}>
                 {location != "/create" &&
-                    <div style={{ marginRight: "1rem", cursor: 'pointer' }}
+                    <div style={{ marginRight: "1rem", ...style}}
                         onClick={() => setLocation('/create')}>
                         <AddArticle width="24px" height="24px" />
                     </div>
                 }
-                <div style={{cursor: "pointer"}}><UserIcon height="24px" width="24px" /></div>
+                <div style={style}><UserIcon height="24px" width="24px" /></div>
             </div>
         </div>
     );
